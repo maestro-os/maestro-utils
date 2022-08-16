0a4bbc235cc3d65c311d11f4e27a4fea34e24be6
llenotre <llenotre@student.42.fr>
2022-08-16 22:43:20 +0200

    Added status_parser.rs

--- ps/src/process/mod.rs
+++ ps/src/process/mod.rs
@@ -1,16 +1,22 @@
 //! Module implementing process structures.
 
-use std::error::Error;
-use std::fs::DirEntry;
+mod status_parser;
+
+use status_parser::StatusParser;
 use std::fs::ReadDir;
 use std::fs;
-use std::io;
-use std::iter::FilterMap;
 
 /// Structure representing a process.
+#[derive(Default)]
 pub struct Process {
+	/// The process's name.
+	pub name: String,
+
 	/// The process's PID.
 	pub pid: u32,
+	/// The PID of the process's parent.
+	pub ppid: u32,
+
 	/// The process's user ID.
 	pub uid: u32,
 	/// The process's real user ID.
@@ -19,6 +25,7 @@ pub struct Process {
 	pub gid: u32,
 	/// The process's real group ID.
 	pub rgid: u32,
+
 	/// The process's TTY.
 	pub tty: Option<String>,
 }
@@ -43,14 +50,14 @@ impl ProcessIterator {
 	fn next_pid(&mut self) -> Option<Result<u32, ()>> {
 		let entry = match self.files.next()? {
 			Ok(e) => e,
-			Err(e) => return Some(Err(())),
+			Err(_) => return Some(Err(())),
 		};
 
 		let file_name = entry.file_name().into_string();
 
 		match file_name {
 			Ok(file_name) => Some(file_name.parse::<u32>().map_err(| _ | ())),
-			Err(e) => Some(Err(())),
+			Err(_) => Some(Err(())),
 		}
 	}
 }
@@ -70,12 +77,15 @@ impl Iterator for ProcessIterator {
 			// The path to the process's status file
 			let path = format!("/proc/{}/status", pid);
 			// Reading the process's status file
-			let content = fs::read_to_string(path);
+			let content = match fs::read_to_string(path) {
+				Ok(content) => content,
+				Err(_) => continue,
+			};
 
 			// Parsing process status
-			let status_parser = StatusParser::new(content);
+			let status_parser = StatusParser::new(&content);
 			match status_parser.yield_process() {
-				Ok(proc) => return proc,
+				Ok(proc) => return Some(proc),
 
 				// On fail, try next process
 				Err(_) => continue,
---
+++ ps/src/process/status_parser.rs
@@ -0,0 +1,60 @@
+//! This module implements a parser for the status of a process.
+
+use super::Process;
+
+/// The status parser parses the content of the file `/proc/{pid}/status`, where `{pid}` is the pid
+/// of the process.
+pub struct StatusParser<'a> {
+	/// The file's content.
+	content: &'a str,
+}
+
+impl<'a> StatusParser<'a> {
+	/// Creates a new instance with the given file content.
+	pub fn new(content: &'a str) -> Self {
+		Self {
+			content,
+		}
+	}
+
+	/// Creates a process structure using a the content of the file.
+	pub fn yield_process(self) -> Result<Process, ()> {
+		let mut proc = Process::default();
+
+		for line in self.content.split('\n') {
+			// Splitting the line to get the name and value
+			let mut s = line.split(':');
+			let name = s.next().ok_or(())?.to_lowercase();
+			let value = s.next().ok_or(())?.trim();
+			if s.next().is_some() {
+				return Err(());
+			}
+
+			match name.as_str() {
+				"name" => proc.name = value.to_string(),
+
+				"pid" => proc.pid = value.parse::<u32>().map_err(|_| ())?,
+				"ppid" => proc.ppid = value.parse::<u32>().map_err(|_| ())?,
+
+				"uid" => {
+					let mut s = value.split_whitespace();
+
+					proc.uid = s.nth(0).ok_or(())?.parse::<u32>().map_err(|_| ())?;
+					proc.ruid = s.nth(3).ok_or(())?.parse::<u32>().map_err(|_| ())?;
+				},
+				"gid" => {
+					let mut s = value.split_whitespace();
+
+					proc.gid = s.nth(0).ok_or(())?.parse::<u32>().map_err(|_| ())?;
+					proc.rgid = s.nth(3).ok_or(())?.parse::<u32>().map_err(|_| ())?;
+				},
+
+				// TODO tty
+
+				_ => {},
+			}
+		}
+
+		Ok(proc)
+	}
+}
