0fc439163d0b92cd0ef841cede115841bcb44a79
llenotre <llenotre@student.42.fr>
2023-01-30 00:35:05 +0100

    Added dmesg

---
+++ dmesg/src/main.rs
@@ -0,0 +1,18 @@
+//! The `dmesg` command allows to print the kernel's logs.
+
+use std::env;
+use std::fs::File;
+
+/// The path to the kmsg device file.
+const KMSG_PATH: &str = "/dev/kmsg";
+
+fn main() {
+    let _args: Vec<String> = env::args().collect();
+	// TODO parse arguments
+
+	// TODO read non blocking from file
+	// TODO for each line:
+	// - split once with `;`
+	// - split left with `,`, then retrieve time, facility and level
+	// - format and print
+}
