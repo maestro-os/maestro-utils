49d66432fae7839649a1975835ef4dc7e60e2fd8
llenotre <llenotre@student.42.fr>
2023-12-06 23:47:59 +0100

    refactor(login): cleanup

--- login/src/main.rs
+++ login/src/main.rs
@@ -2,11 +2,9 @@
 
 #![feature(never_type)]
 
-use std::collections::HashMap;
 use std::env;
 use std::error::Error;
-use std::ffi::OsString;
-use std::path::PathBuf;
+use std::path::Path;
 use std::process::exit;
 use std::process::Command;
 use std::time::Duration;
@@ -32,46 +30,44 @@ fn switch_user(logname: &str, user: &User) -> Result<!, Box<dyn Error>> {
     // Changing user
     user::set(*uid, *gid)?;
 
-    let mut env = env::vars_os().collect::<HashMap<OsString, OsString>>();
-    env.insert("HOME".into(), home.into());
-    env.insert("LOGNAME".into(), logname.into());
-
     // TODO Execute without fork
     // Running the user's program
     let status = Command::new(interpreter)
         .current_dir(home)
-        .envs(env)
+        .env("HOME", home)
+        .env("LOGNAME", logname)
         .status()
-        .map_err(|_| format!("login: Failed to run shell `{}`", interpreter))?;
+        .map_err(|_| format!("login: Failed to run shell `{interpreter}`"))?;
 
     // Exiting with the shell's status
     exit(status.code().unwrap());
 }
 
 fn main() {
-    let _args: Vec<String> = env::args().collect(); // TODO Parse and use
+    let _args = env::args(); // TODO Parse and use
 
     loop {
         println!();
 
-        // Getting user prompt
+        // Get user prompt
         let user_prompt = format!("{} login: ", util::get_hostname());
 
-        // Prompting login and password
+        // Prompt login and password
         let login = prompt(Some(&user_prompt), false).unwrap_or_else(|| exit(1));
         let pass = prompt(None, true).unwrap_or_else(|| exit(1));
 
-        // Reading users lists
-        let passwd = user::read_passwd(&PathBuf::from(user::PASSWD_PATH)).unwrap_or_else(|_| {
-            eprintln!("Cannot read passwd file!");
+        // Read users lists
+        let passwd = user::read_passwd(Path::new(user::PASSWD_PATH)).unwrap_or_else(|e| {
+            eprintln!("Cannot read passwd file: {e}");
             exit(1);
         });
-        let shadow = user::read_shadow(&PathBuf::from(user::SHADOW_PATH)).ok();
+        let shadow = user::read_shadow(&Path::new(user::SHADOW_PATH)).ok();
 
-        // Getting user from prompted login
+        // Get user from prompted login
         let user_entry = passwd.into_iter().find(|e| e.login_name == login);
 
-        util::exec_wait(Duration::from_millis(1000), || {
+        let interval = Duration::from_millis(1000);
+        util::exec_wait(interval, || {
             if let Some(user_entry) = user_entry {
                 // Checking password against user entry
                 let correct = user_entry.check_password(&pass).unwrap_or_else(|| {
@@ -89,7 +85,7 @@ fn main() {
 
                 if correct {
                     switch_user(&login, &user_entry).unwrap_or_else(|e| {
-                        eprintln!("login: {}", e);
+                        eprintln!("login: {e}");
                         exit(1);
                     });
                 }
